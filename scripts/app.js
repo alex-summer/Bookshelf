fad2eb7db4d873336cf87edbc85068944cc393d1
summerag <alex.summer48@yahoo.com>
2020-02-03 15:47:26 -0500

    making the books look pretty

--- scripts/app.js
+++ scripts/app.js
@@ -1,15 +1,25 @@
 let myLibrary = [];
-const theHobbit = new Book("The Hobbit", "Toiklen", "200", "Bilbo goes on an adventure", "./assets/Hobbit.jpg")
-const lotr = new Book("Lord of the Rings", "Toiklen", "300", "Battle for the ring", "./assets/lotr.jpg")
+const theHobbit = new Book("The Hobbit", "J.R.R Toiklen", "200", "Bilbo goes on an adventure", "./assets/Hobbit.jpg")
+const lotr = new Book("Lord of the Rings", "J.R.R Toiklen", "300", "Nice", "./assets/lotr.jpg")
+const lotr2 = new Book("Lord of the Rings", "J.R.R Toiklen", "300", "Nice", "./assets/lotr.jpg")
 addBookToLibrary(theHobbit);
 addBookToLibrary(lotr);
+addBookToLibrary(lotr2);
+addBookToLibrary(lotr2);
+addBookToLibrary(lotr2);
+addBookToLibrary(theHobbit);
+addBookToLibrary(lotr2);
+addBookToLibrary(lotr2);
+addBookToLibrary(theHobbit);
+addBookToLibrary(lotr2);
+addBookToLibrary(lotr2);
 displayLibrary();
 
 
 function Book(title, author, length, description, cover){
   this.title = title;
-  this.author = author;
-  this.length = length;
+  this.author = `Author: ${author}`;
+  this.length = `Length: ${length}`;
   this.description = description;
   this.cover = cover;
 }
@@ -22,21 +32,31 @@ function displayLibrary(){
   myLibrary.forEach(element => {
     const gridDiv = document.getElementById("content");
     var bookDiv = document.createElement("div");
-    bookDiv.className = "gridItem"
     var titleDiv = document.createElement("h2");
     var coverDiv = document.createElement("img");
     var authorDiv = document.createElement("p");
     var lengthDiv = document.createElement("p");
+    var descriptionDiv = document.createElement("p");
+    bookDiv.className = "book"
+    titleDiv.className = "title"
+    coverDiv.className = "cover"
+    authorDiv.className = "author"
+    lengthDiv.className = "length"
+    descriptionDiv.className = "description"
+
+
+    
     titleDiv.textContent = element.title;
     coverDiv.src = element.cover;
-    coverDiv.className = "cover"
     authorDiv.textContent = element.author;
-    lengthDiv.textContent = element.length
+    lengthDiv.textContent = element.length;
+    descriptionDiv.textContent = element.description;
     
     bookDiv.appendChild(titleDiv);
     bookDiv.appendChild(coverDiv);
     bookDiv.appendChild(authorDiv);
     bookDiv.appendChild(lengthDiv);
+    bookDiv.appendChild(descriptionDiv);
     gridDiv.appendChild(bookDiv);
   });
 }
